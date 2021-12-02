0ee9b84ad50d36cafebee899d17484b204dc7292
yesong choi <to06109@naver.com>
2021-12-03 02:56:41 +0900

    로그인, 회원가입 DB연동 전

---
+++ index.js
@@ -0,0 +1,48 @@
+//////express 실행
+const express = require('express')
+const app = express()
+const port = 3000
+
+const goodsRouter = require('./src/routes/goods'); //goods.js 파일에 있는 라우터들을 가져옴
+const userRouter = require('./src/routes/user'); //user.js 파일에 있는 라우터들을 가져옴
+
+//POST나 PUT으로 받을 때 안에 body데이터 쓸 수 있게 하는 미들웨어
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(express.static('public')); //사진, 동영상 등 정적파일 불러오기
+
+app.use('/goods', goodsRouter)
+app.use('/user', userRouter)
+
+/*
+app.use((req, res, next) => { //middleware: 요청이 라우터에 들어오기 전에 req를 체크
+  console.log(req);
+  next();//미들웨어 종료
+});
+*/
+
+//////템플릿 엔진 ejs 사용하기
+app.set('views', __dirname + '/src/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(`${__dirname}/src/public`)); // /js 파일로 연결할 수 있게 해주는 미들웨어
+
+app.get('/', (req, res) => {
+  res.render('main');
+})
+
+//////router: 각각의 주소가 들어왔을 때 어떤 것을 응답해줄지를 설정
+/*
+app.get('/hi', (req, res) => { //GET: 일반 브라우저에서 요청하는 행위
+    res.send('Hi. This is express router')
+  })
+
+app.get('/', (req, res) => {
+  res.send('Hello World! <a href="/hi">Say Hi!</a>') //Say Hi! 링크 누르면 /hi 로 이동
+})
+*/
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`)
+})
+
---
+++ src/public/js/home/login.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//html에 입력된 아이디와 비밀번호를 js에서 제어해야함
+//DOM-> javascript에서 html에 존재하는 데이터들을 기져와 제어할 수 있게하는 인터페이스 
+//const id = document.querySelector("선택자");
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("#loginbtn"),
+    registerBtn = document.querySelector("#registerbtn");
+
+loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", gotoregister);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    
+    //아이디, 비밀번호 서버에 전달
+    //fetch(전달할 경로, 전달할 데이터)
+    fetch("/login", {
+        method: "POST", //body로 데이터 전달할 때는 POST로 전달해야함
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req) //req를 JSON형태로 감싸줌
+    });
+}
+
+function gotoregister() {
+    location.href = "/user/register";
+}
