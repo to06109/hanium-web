8f7ba82ba831eb30797f2db3f4b06ba2c6fba21a
yesong <to06109@naver.com>
2021-12-05 18:28:47 +0900

    User 모델 만들기

--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict"
 
+const UserStorage = require("../../models/UserStorage")
+
 const output = {
     login: (req, res) => {
         res.render('login'); //index.ejs를 그림
@@ -10,30 +12,30 @@ const output = {
     },
 };
 
-//데이터베이스
-const users = {
-    id: ["yesong", "song", "to06109"],
-    psword: ["1234", "5678", "yesong"],
-};
+
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-            psword = req.body.psword;
+        const user = new User(req.body);
+        const response = user.login(); //User에서 res 받음
+        return res.json(response); //받은 res를 client한테 json형태로 전달
+        // const id = req.body.id,
+        //   psword = req.body.psword;
 
-        if (users.id.includes(id)){
-            const idx = users.id.indexOf(id); //id의 인덱스 가져옴
-            if(users.psword[idx] === psword) { //비밀번호 인증
-                return res.json({
-                    success: true,
-                });
-            }
-        }
+        // const users = UserStorage.getUsers("id","psword"); //은닉화된 users 받아옴
+    
+        // const response = {};
+        // if (users.id.includes(id)){
+        //     const idx = users.id.indexOf(id); //id의 인덱스 가져옴
+        //     if(users.psword[idx] === psword) { //비밀번호 인증
+        //         response.success = true;
+        //         return res.json(response);
+        //     }
+        // }
 
-        return res.json({
-            success: false,
-            msg: "로그인에 실패하셨습니다.",
-        });
+        // response.success = false;
+        // response.msg = "로그인에 실패하셨습니다.";
+        // return res.json(response);
     },
 };
 
